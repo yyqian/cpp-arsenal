7ef889be99338881295f234633a37f5863590722
Yinyin Qian <qianyinyin@patsnap.com>
2017-09-14 18:20:19 +0800

    suggest way

--- src/test/string_util_test.cpp
+++ src/test/string_util_test.cpp
@@ -11,6 +11,7 @@ TEST(string, wchar) {
   wstring source1 = L"A\u4E2D"; // UTF-16 encoded
   char source2[] = u8"A\u4E2D"; // UTF-8 encoded, size: 1 + 3 + 1 = 5
   string source3 = u8"A\u4E2D"; // UTF-8 encoded
+  string source4 = "A中"; //推荐的写法
 #ifdef _WIN32
   EXPECT_EQ(2, sizeof(wchar_t));
   EXPECT_EQ(2 * 3, sizeof(source0));
@@ -19,6 +20,7 @@ TEST(string, wchar) {
   EXPECT_EQ("00 41 4E 2D", Hexify(source1.c_str()));
   EXPECT_EQ("41 E4 B8 AD", Hexify(source2));
   EXPECT_EQ("41 E4 B8 AD", Hexify(source3.c_str()));
+  EXPECT_EQ("41 E4 B8 AD", Hexify(source4.c_str()));
   string source1_transform = Narrow(source1);
   wstring source3_transform = Widen(source3);
   EXPECT_EQ("41 E4 B8 AD", Hexify(source1_transform.c_str()));
