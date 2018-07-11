0142ff6428cb8297f039345ae7b5a924dbf674ff
Yinyin Qian <yyqian@live.com>
2018-07-11 23:51:09 +0800

    auto-sync

--- src/apue/file.cpp
+++ src/apue/file.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
+#include <sys/stat.h>
 
 using namespace std;
+
+void print_file(const char *filename) {
+  struct stat f_stat;
+  stat(filename, &f_stat);
+  cout << "birth: " << endl;
+  if (S_ISCHR(f_stat.st_mode)) {
+    cout << "/dev/null is char special file" << endl;
+  }
+}
+
 int main(int argc, char **argv) {
   cout << "Hello" << endl;
+  print_file("/dev/null");
   if(link("/Users/yyqian/tst", "/Users/yyqian/tst_lnk") != 0) {
     cout << "link failed" << endl;
   }
