e15a714b9a150e93ccdbb7e3db83097c8d67d92c
Yinyin Qian <yyqian@live.com>
2018-09-15 15:26:09 +0800

    sync

--- src/unp/tcp_echo_server.c
+++ src/unp/tcp_echo_server.c
@@ -5,12 +5,54 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
-void str_echo(int sockfd) {
+#define MAX_BUFF_SIZE 100
+char global_buf[MAX_BUFF_SIZE];
+
+void echo(int connfd) {
   ssize_t n;
-  char buf[1500];
-  while ((n = read(sockfd, buf, 1500)) > 0) {
-    write(sockfd, buf, n);
+  char buf[MAX_BUFF_SIZE];
+  while ((n = read(connfd, buf, MAX_BUFF_SIZE)) > 0) {
+    printf("received data\n");
+    write(connfd, buf, n);
+  }
+}
+
+const char *read_oneline(int connfd) {
+  ssize_t n = read(connfd, global_buf, MAX_BUFF_SIZE);
+  printf("received: ");
+  for (int i = 0; i < n; ++i) {
+    char c = global_buf[i];
+    printf("%d ", c);
+    if (c == '\r' || c == '\n') {
+      global_buf[i] = '\0';
+    }
+  }
+  printf("\n");
+  return (const char *)global_buf;
+}
+
+void print_file(int connfd, const char *filename) {
+  printf("reading file: [%s]\n", filename);
+  int filefd = open(filename, O_RDONLY);// <fcntl.h> | open is system call, low level, not portable | fopen is c lib function, portable, high level, buffer io
+  if (filefd <= 0) {
+    printf("File not found\n");
+  }
+  char buf[MAX_BUFF_SIZE];
+  ssize_t n;
+  while ((n = read(filefd, buf, MAX_BUFF_SIZE)) > 0) {
+    printf(".");
+    write(connfd, buf, n);
+  }
+  printf("\n");
+  close(filefd);
+}
+
+void process_connfd(int connfd) {
+  while(1) {
+    const char * filename = read_oneline(connfd);
+    print_file(connfd, filename);
   }
 }
 
@@ -29,11 +71,15 @@ int main(int argc, char **argv) {
   for (;;) {
     chilen = sizeof(cliaddr);
     connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &chilen); // 客户端三次握手完成后，accept 返回
+    printf("Three-way handshake finished. Connection is established.\n");
     if ((childpid = fork()) == 0) { // fork 子进程
+      printf("Child process will close listenfd\n");
       close(listenfd); // 子进程关闭 listenfd，引用数减一
-      str_echo(connfd); // business logic
+      process_connfd(connfd); // business logic
+      printf("Child process will exit\n");
       exit(0); // 子进程退出
     }
+    printf("Parent process will close connfd\n");
     close(connfd); // 父进程关闭 connfd，引用数减一
   }
 }
