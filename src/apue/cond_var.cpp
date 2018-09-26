7ae8bb07cfb7f923fc7ced7eaa4374d0a367863a
Yinyin Qian <yyqian@live.com>
2018-09-26 20:17:23 +0800

    sync

--- src/apue/cond_var.cpp
+++ src/apue/cond_var.cpp
@@ -1,7 +1,10 @@
 #include <pthread.h>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <unistd.h>
+
+using namespace std;
 
 bool ready = false;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -27,7 +30,19 @@ void *p1(void *arg) {
   return NULL;
 }
 
-int main() {
+bool done = false;
+
+void *child(void *arg) {
+  pthread_mutex_lock(&lock);
+  cout << "child get lock" << endl;
+  done = true;
+  pthread_cond_signal(&cond);
+  cout << "child signaled" << endl;
+  pthread_mutex_unlock(&lock);
+}
+
+int main(int argc, char **argv) {
+  // cv0
   pthread_t pt0;
   pthread_t pt1;
   pthread_create(&pt0, NULL, p0, NULL);
@@ -35,5 +50,15 @@ int main() {
   pthread_create(&pt1, NULL, p1, NULL);
   pthread_join(pt0, NULL);
   pthread_join(pt1, NULL);
+  // cv1
+  pthread_t pt;
+  pthread_create(&pt, nullptr, child, nullptr);
+  pthread_mutex_lock(&lock);
+  while (!done) {
+    cout << "main will wait" << endl;
+    pthread_cond_wait(&cond, &lock);
+    cout << "main is wakeup" << endl;
+  }
+  pthread_mutex_unlock(&lock);
   return EXIT_SUCCESS;
 }
